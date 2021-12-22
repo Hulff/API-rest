e5ef2ab93e6777d8151e5e0b1dfef6d8608ce60c
Hulff <ph2717693@Gmail.com>
2021-12-22 10:16:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 8080
+
+app.use( express.json() )
+
+
+app.get('/tshirt',(req,res)=>{
+    res.status(200).send({
+        tshirt: "blank",
+        size: "big"
+    }
+    )
+});
+
+app.post('/tshirt/:id',(req,res)=>{
+    const {id} = req.params
+    const {logo} = req.body
+
+    if (!logo) {
+        res.status(418).send( {message: 'we need a logo'})
+    }
+
+    res.send({
+        tshirt: `tshirt with ${logo} and ID of ${id}`,
+    });
+});
